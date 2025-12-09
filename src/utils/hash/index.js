2bfd3e887a3cbbbc0fb8ba8d5170e5097a3698c8
Mahd Mohamed <engmahd00@gmail.com>
2025-12-09 16:25:59 +0200

    solve issue of hased password

--- src/utils/hash/index.js
+++ src/utils/hash/index.js
@@ -1,10 +1,11 @@
 import bcrypt from "bcrypt";
-import config from "../../../config/dev.config.js";
 
 export const hashPassword = async (password) => {
     return await bcrypt.hash(password, 10);
 };
 
-export const comparePassword = (password, hashedPassword=config.HASHED_PASSWORD) => {
+// Compare user-supplied password to stored hash; fail closed if hash is missing
+export const comparePassword = async (password, hashedPassword) => {
+    if (!hashedPassword) return false;
     return bcrypt.compare(password, hashedPassword);
 };
